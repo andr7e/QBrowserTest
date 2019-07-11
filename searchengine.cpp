a7179d61c005fd3d3f8450a73997502b10886970
andr7e <andrey@andrey-HP-Z400-Workstation.(none)>
2019-07-11 18:00:13 +0300

    Test search engine

--- searchengine.cpp
+++ searchengine.cpp
@@ -17,5 +17,14 @@ QUrl SearchEngine::getUrl(const QString &searchText)
     urlQuery.addQueryItem(QLatin1String("client"), QLatin1String("qtdemobrowser"));
     url.setQuery(urlQuery);
 
+    /*
+    QUrl url(QLatin1String("https://duckduckgo.com"));
+    QUrlQuery urlQuery;
+    urlQuery.addQueryItem(QLatin1String("q"), searchText);
+    urlQuery.addQueryItem(QLatin1String("ie"), QLatin1String("UTF-8"));
+    urlQuery.addQueryItem(QLatin1String("oe"), QLatin1String("UTF-8"));
+    url.setQuery(urlQuery);
+    */
+
     return url;
 }
